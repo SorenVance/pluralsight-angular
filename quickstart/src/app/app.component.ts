dc7b01010c0bd8d89fe871a2b35e0e07837e6532
Steve Kramer <steve.kramer@thomsonreuters.com>
2017-09-08 14:48:43 -0500

    Get ngModel to work

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent  {
-  //[] = property binding - Component to Dom
-  //() = event binding - Dom to Component
-  title = 'Angular';
-  name = 'Steve';
-  myColor = 'red';
-  changeColor() {
-    this.myColor = this.myColor === 'blue' ? 'red' : 'blue';
-  }
+    //[] = property binding - Component to Dom
+    //() = event binding - Dom to Component
+    //[()] = Circular Component to Dom to Component
+    title = 'Angular';
+    name = 'Steve';
+    myColor = 'red';
+    changeColor() {
+        this.myColor = this.myColor === 'blue' ? 'red' : 'blue';
+    };
 }
